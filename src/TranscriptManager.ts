007ee86f042c72c642d743424ce668a79357e4b7
Azamat Zakirovch <a.eshqoziyev@newuu.uz>
2025-09-13 21:52:43 +0500

    - TranscriptManager.ts file is edited. New class methods created.
     - loadData() -> returns the database in list type.
     - debugPrint() -> you can check the database that imported or no.
     - initialize() -> load data from database. If database is empty it creates itself.
     - saveData() -> to save edited data to database.
     - getAll() -> this method is returns all data from data.json file.
     - addStudent() -> this method is created for add new student with new student name. Grades added after a while.
     - deleteStudent() -> deletes all data of student with student ID.
     - addGrade() -> this method will add the grade if student with using studentID, Course and adds the grade.
     - getGrade() -> this method is returns the grade of student using student ID and Course.
    
    © Azamat E. Zakirovch

--- src/TranscriptManager.ts
+++ src/TranscriptManager.ts
@@ -4,5 +4,30 @@ import {Transcript, Course, CourseGrade, StudentID, Student} from "./types";
 const database = "./data.json";
 
 export class TranscriptManager {
+    //@ts-ignore
+    private transcripts: Transcript[];
 
+    constructor(){}
+
+    private loadData(): Transcript[] {
+        return []
+    }
+
+    public initialize(): void{}
+
+    private saveData():void{}
+
+    public getAll(): Transcript[]{
+        return []
+    }
+
+    public addStudent(name: string):void{}
+
+    public deleteStudent(studentID:string):void{}
+
+    public addGrade(studentID: StudentID, course: Course, grade : number):void{}
+
+    public getGrade(studentId: StudentID, course: Course):number{
+        return 1
+    }
 }
