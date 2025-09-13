c091bc92c62d36c6a36f268e4859c972f27d6bb6
Azamat Zakirovch <a.eshqoziyev@newuu.uz>
2025-09-13 22:36:00 +0500

    - TranscriptManager.ts file is edited. initialize(), constructor(), saveData(), getAll(), addStudent() methods edited.
     -initialize() -> initialize the data from database. if there is no data in data.json file automatically creates 4 new student with no grades.
    
     -constructor() -> returns the data to transcripts.
    
     -saveData() -> save the data after edition to database.
    
     -getAll() -> get all data from database.
    
     - addStudent() adds new student with no grades. new ID is generated automatically.
    
    © Azamat E. Zakirovch

--- src/TranscriptManager.ts
+++ src/TranscriptManager.ts
@@ -4,10 +4,12 @@ import {Transcript, Course, CourseGrade, StudentID, Student} from "./types";
 const database = "./data.json";
 
 export class TranscriptManager {
-    //@ts-ignore
+
     private transcripts: Transcript[];
 
-    constructor(){}
+    constructor(){
+        this.transcripts = this.loadData();
+    }
 
     private loadData(): Transcript[] {
 
@@ -21,17 +23,55 @@ export class TranscriptManager {
         return JSON.parse(data);
     }
 
-    public initialize(): void{}
+    public initialize(): void{
+        if(this.transcripts.length > 0){
+            console.log("Initializing Transcript");
+        }
 
-    private saveData():void{}
-
-    public getAll(): Transcript[]{
-        return []
+        this.transcripts = [
+            {
+                student:{studentID:230001, studentName: "Sherzod"},
+                grades: []
+            },
+            {
+                student:{studentID:230002, studentName: "Olimjon"},
+                grades: []
+            },
+            {
+                student:{studentID:230003, studentName: "Leyla"},
+                grades: []
+            },
+            {
+                student:{studentID:230004, studentName: "Umarjon"},
+                grades: []
+            }
+        ]
+        this.saveData()
+        console.log("The Database initialized with 4 students");
     }
 
-    public addStudent(name: string):void{}
+    private saveData():void{
+        const jsonData = JSON.stringify(this.transcripts, null);
+        fs.writeFileSync(database, jsonData,"utf-8");
+    }
 
-    public deleteStudent(studentID:string):void{}
+    public getAll(): Transcript[]{
+        return this.transcripts;
+    }
+
+    public addStudent(name: string):number{
+        const newID =
+            this.transcripts.length > 0 ? Math.max(...this.transcripts.map((t) => t.student.studentID)) + 1:230001;
+
+        const newStudent: Student = {studentID: newID, studentName: name};
+        this.transcripts.push({student:newStudent, grades: []});
+        this.saveData();
+        return newID;
+    }
+
+    public deleteStudent(studentID:string):void{
+
+    }
 
     public addGrade(studentID: StudentID, course: Course, grade : number):void{}
 
